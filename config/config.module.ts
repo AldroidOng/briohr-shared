8eb66f042ddb029d40ad69b26365522c01efcfde
Alvin Ong <alvin.ong@instahome.com>
2024-08-14 04:33:11 +0800

    Add extra config validation

--- config/config.module.ts
+++ config/config.module.ts
@@ -1,5 +1,3 @@
-// This is a wrapper around the NestJS Config module, allowing us to load environment variables from a .env file.
-
 import { Module } from '@nestjs/common';
 import {
   ConfigService,
@@ -10,12 +8,18 @@ import * as Joi from 'joi';
 @Module({
   imports: [
     NestConfigModule.forRoot({
+      isGlobal: true,
       validationSchema: Joi.object({
         MONGODB_URI: Joi.string().required(),
+        NOTIFICATION_MICROSERVICE_HOST: Joi.string().default('localhost'),
+        NOTIFICATION_MICROSERVICE_PORT: Joi.number().default(3001),
+        PROFILE_MICROSERVICE_HOST: Joi.string().default('localhost'),
+        PROFILE_MICROSERVICE_PORT: Joi.number().default(3002),
+        // Add other environment variables and their validation rules here
       }),
     }),
   ],
   providers: [ConfigService],
-  exports: [ConfigService],
+  exports: [NestConfigModule, ConfigService], // Ensure both are exported
 })
 export class ConfigModule {}
